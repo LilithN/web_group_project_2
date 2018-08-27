fd37045b5490be9b86330439ec44162b5b240fc5
Samantha Goodson <Shad0wech00@gmail.com>
2018-08-27 16:36:16 -0500

    javascript done

--- JScript/menue.js
+++ JScript/menue.js
@@ -1,4 +1,17 @@
-$(".cakeButtonOn").show();
-$("#cakeButtonOff").hide();
-$(".otherButtonOn").hide();
-$("#OtherButtonOff").show();
+$("#otherButton").click(function(){
+    $(".other").show();
+    $(".cakes").hide();
+    $("#otherButton").addClass("otherButtonOn");
+    $("#otherButton").removeClass("otherButtonOff");
+    $("#cakeButton").addClass("cakeButtonOff");
+    $("#cakeButton").removeClass("cakeButtonOn");
+});
+
+$("#cakeButton").click(function(){
+    $(".cakes").show();
+    $(".other").hide();
+    $("#cakeButton").removeClass("cakeButtonOff");
+    $("#cakeButton").addClass("cakeButtonOn");
+    $("#otherButton").removeClass("otherButtonOn");
+    $("#otherButton").addClass("otherButtonOff");
+});
